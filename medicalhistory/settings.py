2d951da3a09cff3d0f722565dabd5ab6092ca9bb
Geetha <atgeethapriya2004@gmail.com>
2025-12-05 12:37:24 +0530

    Vercel fix

--- medicalhistory/settings.py
+++ medicalhistory/settings.py
@@ -48,8 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
 ROOT_URLCONF = 'medicalhistory.urls'
 
 TEMPLATES = [
