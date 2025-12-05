981a2e3c3e9fa4d99625e650ebe31e2fb6c18f0f
Geetha <atgeethapriya2004@gmail.com>
2025-12-05 13:06:29 +0530

    Vercel fix

--- medicalhistory/settings.py
+++ medicalhistory/settings.py
@@ -47,14 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # your app
-    'medicalhistory',
+    'extractions',
 ]
-
-# ---------------------------------------------------------
-# MIDDLEWARE
-# ---------------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -64,20 +58,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-ROOT_URLCONF = 'lifespring.urls'
-
-# ---------------------------------------------------------
-# TEMPLATES
-# ---------------------------------------------------------
+ROOT_URLCONF = 'medicalhistory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': ["C:/Users/GEETHA PRIYA/Desktop/medical data app/medicalhistory/extractions/templates"],  # <- added comma
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -86,60 +74,49 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lifespring.wsgi.application'
-
-# ---------------------------------------------------------
-# DATABASE (SQLite works on Vercel)
-# ---------------------------------------------------------
+WSGI_APPLICATION = 'medicalhistory.wsgi.application'
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# ---------------------------------------------------------
-# PASSWORD VALIDATION
-# ---------------------------------------------------------
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
-
-# ---------------------------------------------------------
-# INTERNATIONALIZATION
-# ---------------------------------------------------------
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-# ---------------------------------------------------------
-# STATIC FILES
-# ---------------------------------------------------------
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"   # Vercel collects here
-
-# ---------------------------------------------------------
-# MEDIA FILES
-# ---------------------------------------------------------
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "extractions" / "static"]
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# ---------------------------------------------------------
-# EMAIL SETTINGS (Gmail)
-# ---------------------------------------------------------
+# EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'atgeethapriya2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'khzkrmzjsybnlsuo'  # Gmail App Password ONLY
+EMAIL_HOST_USER = 'atgeethapriya2004@gmail.com'           # <-- replace this
+EMAIL_HOST_PASSWORD = 'khzkrmzjsybnlsuo'          # <-- replace this (App Password, not Gmail login)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
